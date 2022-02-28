1b5ae72da666c7cc29bad308648de81a62e8c40b
Safayet Mahid <safayetmahid96@gmail.com>
2022-02-28 17:10:40 +0600

    portfolio services and experience sections are added

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,90 @@
 import React from 'react';
 import "./Portfolio.css"
+import CopaAmerica from "../assets/copa_america.png"
+import Hovers from "../assets/design_hovers.png"
+import GlassMorphism from "../assets/glass-morphism.png"
+import Medicenter from "../assets/medicenter.png"
+import SunShades from "../assets/sun_shades.png"
+
+
+const portfolio = [
+    {
+        id: 1,
+        Image: CopaAmerica,
+        title: "Copa America",
+        github_client: "https://github.com/Safayet-Mahid/healthcare-website",
+        github_server: null,
+        live: "https://safayet-mahid.github.io/copa-america-2021/"
+    },
+    {
+        id: 2,
+        Image: Hovers,
+        title: "Hover Effects",
+        github_client: "https://github.com/Safayet-Mahid/hovers",
+        github_server: null,
+        live: "https://safayet-mahid.github.io/hovers/"
+    },
+    {
+        id: 3,
+        Image: GlassMorphism,
+        title: "Glass Morphism",
+        github_client: "https://safayet-mahid.github.io/glass-morphism/",
+        github_server: null,
+        live: "https://safayet-mahid.github.io/copa-america-2021/"
+    },
+    {
+        id: 4,
+        Image: Medicenter,
+        title: "Medicenter",
+        github_client: "https://github.com/Safayet-Mahid/healthcare-website",
+        github_server: null,
+        live: "https://github.com/Safayet-Mahid/healthcare-website"
+    },
+    {
+        id: 5,
+        Image: SunShades,
+        title: "Sun Shades",
+        github_client: "https://github.com/Safayet-Mahid/sun-shades",
+        github_server: "https://github.com/Safayet-Mahid/sun-shades-server",
+        live: "https://sunglass-website-90881.web.app/"
+    },
+]
 
 const Portfolio = () => {
     return (
-        <div>
+        <section id='portfolio'>
+            <h5>My Recent Works</h5>
+            <h2>Portfolio</h2>
+            <div className='container portfolio__container'>
+                {
+                    portfolio.map(({ id, Image, title, github_client, github_server, live }) => {
+                        return (
+                            <article key={id} className='portfolio__item'>
+                                <div className='portfolio__item-image'>
+                                    <img src={Image} alt="" />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className='portfolio__item-cta'>
+                                    {
+                                        github_server === null ?
+                                            <a href={github_client} className='btn' target="blank"> Repo </a> :
+                                            <>
+                                                <a href={github_client} className='btn' target="blank"> CLient </a>
+                                                <a href={github_server} className='btn ' target="blank"> Server </a>
+                                            </>
 
-        </div>
+                                    }
+                                    <a href={live} className='btn btn-primary' target="blank"> Live </a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+
+            </div>
+
+
+        </section>
     );
 };
 
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,11 +1,123 @@
 import React from 'react';
 import "./Services.css"
+import { BiCheck } from "react-icons/bi"
 
 const Services = () => {
     return (
-        <div>
+        <section id='services'>
+            <h5>What I Offer</h5>
+            <h2>My Services</h2>
 
-        </div>
+            <div className="container services__container">
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+
+                    </ul>
+                </article>
+
+                {/* end of Ui/Ux */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+
+                    </ul>
+                </article>
+
+                {/* end of Web Developemnt */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Consultency</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Lorem dolor sit amet consectetur adipisicing.</p>
+                        </li>
+
+                    </ul>
+                </article>
+
+                {/* COnsultensy */}
+            </div>
+
+        </section>
     );
 };
 
